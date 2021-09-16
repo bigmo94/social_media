1a8f0b43dce29a204d10cdefc604d479b26fe2e8
bigmo <m.rezaie94@yahoo.com>
2021-09-16 13:04:43 +0430

    Create PV Messages model

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+
+class PVMessage(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='pm_sender')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='pm_receiver')
+    body = models.TextField(_('body'))
+
+    class Meta:
+        db_table = 'messages'
+        verbose_name = _('message')
+        verbose_name_plural = _('messages')
+
