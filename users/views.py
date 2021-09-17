63adfb85a6a6afa394af7e5e9a69a820eeac1e68
bigmo <m.rezaie94@yahoo.com>
2021-09-17 12:07:15 +0430

    Create api view for Profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+
+from .models import Profile
+from .serializer import (ProfileSerializer,
+                         ProfileRegisterSerializer,
+                         ProfileLoginSerializer,
+                         ProfileVerifySerializer)
+
+
+class ProfileRegisterAPIView(generics.CreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileRegisterSerializer
+
+
+class ProfileVerifyAPIView(generics.CreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileVerifySerializer
+
+
+class ProfileLoginAPIView(generics.CreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileLoginSerializer
+
+
+class ProfileRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'pk'
