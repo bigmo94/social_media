876c7e8b7dbce6759d468accd9f27020cb59653c
bigmo <m.rezaie94@yahoo.com>
2021-09-17 14:53:58 +0430

    Fix the sending mail function

--- users/utils.py
+++ users/utils.py
@@ -2,4 +2,5 @@ from django.core.mail import send_mail
 
 
 def send_verification_code(profile_email, verification_code):
-    send_mail('Verification code', verification_code, 'yara company', [profile_email], fail_silently=False)
+    send_mail(subject='Verification code', message=str(verification_code), from_email='yara company',
+              recipient_list=[profile_email], fail_silently=False)
