3fc1844b0619620fd1115a7e9bf445658a72f397
bigmo <m.rezaie94@yahoo.com>
2021-09-18 18:16:58 +0430

    Add url for editing

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListAPIView, PostDetailAPIView
+from .views import PostListAPIView, PostDetailAPIView, PostRetrieveUpdateDestroyAPIView
 
 app_name = "posts"
 
 urlpatterns = [
     path('create/', PostListAPIView.as_view(), name='post_create'),
     path('<int:pk>/', PostDetailAPIView.as_view(), name='post_detail'),
+    path('edit/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='post_edit')
 ]
