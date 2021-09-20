2366fb48a2e08adadfbfc86e72dc909f176471b5
bigmo <m.rezaie94@yahoo.com>
2021-09-20 11:45:31 +0430

    Add pagination and permission

--- social_network/settings.py
+++ social_network/settings.py
@@ -114,10 +114,14 @@ EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':
+        10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    #    'DEFAULT_PERMISSION_CLASSES': (
-    #         'rest_framework.permissions.IsAdminUser'
-    #    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    ),
 }
