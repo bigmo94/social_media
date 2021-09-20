fc89fea840b8373b5aa8203a604dcf16532cc892
bigmo <m.rezaie94@yahoo.com>
2021-09-20 14:19:22 +0430

    Fix bug on default permission

--- social_network/settings.py
+++ social_network/settings.py
@@ -122,6 +122,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
