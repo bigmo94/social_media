9ba43b1ef5b9e6ff91aca3e7f7f9fcf39c986014
bigmo <m.rezaie94@yahoo.com>
2021-09-20 12:06:07 +0430

    Add permission for Is Owner

--- users/permissions.py
+++ users/permissions.py
@@ -1,10 +1,21 @@
 from rest_framework import status
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 
 
-class IsProfileOwner(BasePermission):
+class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj != request.user:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        return True
+        if request.method in SAFE_METHODS:
+            return True
+
+        if obj == request.user:
+            return True
+
+        return False
+
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.username == request.data.get('receiver') or request.data.get('sender'):
+            return True
+        return False
