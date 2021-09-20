c2a63e8fe58ecda9e354b8e63d554d0d9cdc5a3d
bigmo <m.rezaie94@yahoo.com>
2021-09-20 12:15:40 +0430

    Improve message api view

--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from rest_framework import generics, status, viewsets
+from rest_framework import generics, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.parsers import JSONParser
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
 from .models import Profile, Message, Follow
-from .permissions import IsProfileOwner
+from .permissions import IsOwnerOrReadOnly, IsOwner
 from .serializer import (ProfileSerializer,
                          ProfileRegisterSerializer,
                          ProfileLoginSerializer,
@@ -17,42 +17,39 @@ from .serializer import (ProfileSerializer,
 class ProfileRegisterAPIView(generics.CreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileRegisterSerializer
+    permission_classes = [AllowAny, ]
 
 
 class ProfileVerifyAPIView(generics.CreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileVerifySerializer
+    permission_classes = [AllowAny, ]
 
 
 class ProfileLoginAPIView(generics.CreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileLoginSerializer
+    permission_classes = [AllowAny, ]
 
 
 class ProfileRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsProfileOwner, ]
+    permission_classes = [IsOwnerOrReadOnly, ]
     authentication_classes = (TokenAuthentication,)
     lookup_field = 'pk'
     lookup_url_kwarg = 'pk'
 
 
-def message_view(request, sender=None, receiver=None):
-    if request.method == 'GET':
-        messages = Message.objects.filter(sender_id=sender, receiver_id=receiver)
-        serializer = MessageSerializer(messages, many=True, context={'request': request})
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MessageSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class MessageListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [IsOwner, ]
+
+    def get_queryset(self):
+        return Message.objects.filter(receiver=self.request.user)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowCreateListAPIView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = FollowSerializer
     queryset = Follow.objects.all()
